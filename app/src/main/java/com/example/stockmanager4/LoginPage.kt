8558efcbf5a3689fe656457e0640f2914f11df44
Sdoue61 <sebastiendoue@gmail.com>
2021-08-19 13:52:33 +0400

    sign in + authentification mail ok

--- app/src/main/java/com/example/stockmanager4/LoginPage.kt
+++ app/src/main/java/com/example/stockmanager4/LoginPage.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.stockmanager4.databinding.ActivityLoginPageBinding
 import com.example.stockmanager4.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginPage : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityLoginPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +20,22 @@ class LoginPage : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        auth = Firebase.auth
+
         val register = findViewById<Button>(R.id.BTregister)
         register.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+    }
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        val status = findViewById<TextView>(R.id.TVstatusconnexion)
+        if(currentUser != null){
+            status.text = "ONLINE"
+        }else {
+            status.text = "OFFLINE"
         }
     }
 }
--- app/src/main/java/com/example/stockmanager4/RegisterActivity.kt
+++ app/src/main/java/com/example/stockmanager4/RegisterActivity.kt
@@ -1,11 +1,60 @@
 package com.example.stockmanager4
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.stockmanager4.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+
+        val registeremail = findViewById<EditText>(R.id.ETregisteremail)
+        val registerpwd = findViewById<EditText>(R.id.ETregisterpwd1)
+        val registerpwdconf = findViewById<EditText>(R.id.ETregisterpwd2)
+        val signinbutton = findViewById<Button>(R.id.BTsignup)
+
+        signinbutton.setOnClickListener {
+            val email = registeremail.text.toString()
+            val password = registerpwd.text.toString()
+            val password2 = registerpwdconf.text.toString()
+
+            if (password != password2) {
+                Toast.makeText(baseContext, "Passwords aren't identical", Toast.LENGTH_SHORT).show()
+            } else if (password < 6.toString()) {
+                Toast.makeText(baseContext, "Minimum 6 Characters", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.createUserWithEmailAndPassword(email,
+                    password).addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        user!!.sendEmailVerification().addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(baseContext,
+                                    "Verification email sent",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        val intent = Intent(this, LoginPage::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(baseContext, "Sign In Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
